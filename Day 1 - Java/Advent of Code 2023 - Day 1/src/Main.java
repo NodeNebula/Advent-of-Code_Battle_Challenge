811999307b2966c99a47da196a10898267f4d2d0
NodeNebula <node.nebula@gmail.com>
2023-12-01 21:52:42 +0100

    Day 1 part 1, completed

---
+++ Day 1 - Java/Advent of Code 2023 - Day 1/src/Main.java
@@ -0,0 +1,35 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            File puzzleInput = new File("src/input.txt");
+            Scanner fileReader = new Scanner(puzzleInput);
+
+            int total = 0;
+
+            while (fileReader.hasNextLine()) {
+                String line = fileReader.nextLine();
+
+                String stringNumber = "";
+
+                for (int i = 0; i < line.length(); i++) {
+                    if (Character.isDigit(line.charAt(i))) {
+                        stringNumber += line.charAt(i);
+                    }
+                }
+
+                String currentNumber = String.valueOf(stringNumber.charAt(0)) +
+                        String.valueOf(stringNumber.charAt(stringNumber.length() - 1));
+                total += Integer.parseInt(currentNumber);
+            }
+
+            System.out.println(total);
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
