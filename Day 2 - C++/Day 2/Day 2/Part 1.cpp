0ec96ee716427c05204148d3db4af7f6382ea925
NodeNebula <node.nebula@gmail.com>
2023-12-02 22:35:10 +0100

    Day 2, part 1 finished

--- Day 2 - C++/Day 2/Day 2/Part 1.cpp
+++ Day 2 - C++/Day 2/Day 2/Part 1.cpp
@@ -1,6 +1,65 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int main() {
+    string line;
+    ifstream myfile("input.txt");
+    int currentline{};
+    int total{};
+
+    if (myfile.is_open()) {
+        while (getline(myfile, line)) {
+            // cout << line << "\n";
+
+            istringstream ss(line);
+            string pull;
+            bool hasHigher = false;
+
+            currentline++;
+
+            while (getline(ss, pull, ';')) {
+                int posred = pull.find("red");
+                int posgrn = pull.find("green");
+                int posblu = pull.find("blue");
+
+                const int maxred = 12;
+                const int maxgrn = 13;
+                const int maxblu = 14;
+
+                if (posred != -1) {
+                    if (stoi(pull.substr(posred - 3, 2)) > maxred) {
+                        hasHigher = true;
+                    }
+                }
+                if (posgrn != -1) {
+                    if (stoi(pull.substr(posgrn - 3, 2)) > maxgrn) {
+                        hasHigher = true;
+                    }
+
+                }
+                if (posblu != -1) {
+                    if (stoi(pull.substr(posblu - 3, 2)) > maxblu) {
+                        hasHigher = true;
+                    }
+                }
+            }
+
+            if (!hasHigher) {
+                total += currentline;
+            }
+            // string test = line.substr();
+        }
+        myfile.close();
+    } else cout << "Unable to open file";
+
+    cout << total << "\n";
+    return 0;
 }
+
+// 5050 too high
+
+// 2330 too low
