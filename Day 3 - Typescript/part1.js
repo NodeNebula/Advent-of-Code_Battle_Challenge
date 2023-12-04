d78ad1fe7079ee31ee1c5397dd55a0793483c7d8
NodeNebula <node.nebula@gmail.com>
2023-12-04 15:30:10 +0100

    Day 3, part 1 unfinished

--- Day 3 - Typescript/part1.js
+++ Day 3 - Typescript/part1.js
@@ -1,2 +1,44 @@
-var msg = "Hello, part 1.";
-console.log(msg);
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var fs = require("fs");
+// function checkChar(string: character): boolean {
+//   return ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '-', '+', '[', ']', '{', '}', ';', ':', "'", '"', '\\', '|', ',', '<', '>', '/', '?', '~'].includes(character);
+// }
+var input = fs.readFileSync('test.txt', 'utf-8');
+var lines = input.split('\r\n');
+var lineCount = -1;
+for (var _i = 0, lines_1 = lines; _i < lines_1.length; _i++) {
+    var line = lines_1[_i];
+    lineCount++;
+    var charCount = -1;
+    for (var _a = 0, line_1 = line; _a < line_1.length; _a++) {
+        var char = line_1[_a];
+        charCount++;
+        // console.log(lineCount + "-" + charCount + " " + char + "-" + line[charCount])
+        if (!isNaN(Number(char))) {
+            if (charCount > 0 && !isNaN(Number(line[charCount - 1])))
+                continue;
+            var num = "";
+            num = num + char;
+            if (!isNaN(Number(line[charCount + 1]))) {
+                num = num + line[charCount + 1];
+                if (!isNaN(Number(line[charCount + 2]))) {
+                    num = num + line[charCount + 2];
+                }
+            }
+            console.log(num);
+            var numbersCheck = [];
+            for (var i = -1; i < 1; i++) {
+                if (charCount == 0 && i == -1)
+                    continue;
+                for (var j = -1; j < 2; j++) {
+                    if (lineCount == 0 && j == -1)
+                        continue;
+                    numbersCheck[i + 1] = [];
+                    numbersCheck[i + 1].push(lines[lineCount + j][charCount + i]);
+                }
+            }
+            console.log(numbersCheck);
+        }
+    }
+}
--- Day 3 - Typescript/part1.ts
+++ Day 3 - Typescript/part1.ts
@@ -1,2 +1,48 @@
-let msg : string = "Hello, part 1.";
-console.log(msg);
+import * as fs from 'fs';
+
+// function checkChar(string: character): boolean {
+//   return ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '-', '+', '[', ']', '{', '}', ';', ':', "'", '"', '\\', '|', ',', '<', '>', '/', '?', '~'].includes(character);
+// }
+
+const input = fs.readFileSync('test.txt', 'utf-8');
+const lines = input.split('\r\n');
+
+let lineCount: number = -1;
+for (const line of lines) {
+  lineCount++;
+
+  let charCount: number = -1;
+  for (const char of line) {
+    charCount++;
+    
+      // console.log(lineCount + "-" + charCount + " " + char + "-" + line[charCount])
+    if (!isNaN(Number(char))) {
+      if (charCount > 0 && !isNaN(Number(line[charCount - 1]))) continue;
+
+      let num: string = "";
+
+      num = num + char;
+      if (!isNaN(Number(line[charCount + 1]))) {
+        num = num + line[charCount + 1];
+        if (!isNaN(Number(line[charCount + 2]))) {
+          num = num + line[charCount + 2];
+        }
+      }
+      console.log(num);
+
+      let numbersCheck: string[][] = [];
+
+      for (let i = -1; i < 1; i++) {
+        if (charCount == 0 && i == -1) continue;
+        for (let j = -1; j < 2; j++) {
+          if (lineCount == 0 && j == -1) continue;
+
+          numbersCheck[i + 1] = [];
+          numbersCheck[i + 1].push(lines[lineCount + j][charCount + i]);
+        }
+      }
+      console.log(numbersCheck);
+
+    }
+  }
+}
