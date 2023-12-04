8c4ef660974afe6547976b7cf1887c363e51f26e
NodeNebula <node.nebula@gmail.com>
2023-12-04 19:41:20 +0100

    Day 3, part 2 - Bugged

--- Day 3 - Typescript/part2.js
+++ Day 3 - Typescript/part2.js
@@ -1,2 +1,66 @@
-var msg = "Hello, part 2.";
-console.log(msg);
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var fs = require("fs");
+var input = fs.readFileSync('test.txt', 'utf-8');
+var lines = input.split('\r\n');
+var total = 0;
+var lineCount = -1;
+for (var _i = 0, lines_1 = lines; _i < lines_1.length; _i++) {
+    var line = lines_1[_i];
+    lineCount++;
+    var charCount = -1;
+    for (var _a = 0, line_1 = line; _a < line_1.length; _a++) {
+        var char = line_1[_a];
+        charCount++;
+        if (char != '*')
+            continue;
+        var numberCheck = [];
+        try {
+            for (var i = 0; i < 3; i++) {
+                numberCheck[i] = [];
+                for (var j = 0; j < 7; j++) {
+                    numberCheck[i].push(lines[lineCount + i - 1][charCount + j - 3]);
+                }
+            }
+        }
+        catch (TypeError) { }
+        // console.log(numberCheck);
+        var numberList = [];
+        var nextNum = 0;
+        for (var i = 0; i < numberCheck.length; i++) {
+            for (var j = 0; j < numberCheck[i].length; j++) {
+                if (j < 2 && numberCheck[i][2] === ".")
+                    j = 3;
+                if (j === 4 && numberCheck[i][j] === ".")
+                    break;
+                if (j === 5 && numberCheck[i][j] === ".")
+                    break;
+                if (numberCheck[i][j] === undefined || numberCheck[i][j] === ".")
+                    continue;
+                if (!isNaN(Number(numberCheck[i][j]))) {
+                    numberList[nextNum] = numberCheck[i][j];
+                    if (!isNaN(Number(numberCheck[i][j + 1]))) {
+                        j++;
+                        numberList[nextNum] += numberCheck[i][j];
+                        if (!isNaN(Number(numberCheck[i][j + 1]))) {
+                            j++;
+                            numberList[nextNum] += numberCheck[i][j];
+                            nextNum++;
+                        }
+                        else {
+                            nextNum++;
+                        }
+                    }
+                    else {
+                        nextNum++;
+                    }
+                }
+            }
+        }
+        if (numberList.length < 2)
+            continue;
+        total += Number(numberList[0]) * Number(numberList[1]);
+    }
+}
+console.log(total);
+// 73662618 too low
--- Day 3 - Typescript/part2.ts
+++ Day 3 - Typescript/part2.ts
@@ -1,2 +1,72 @@
-let msg : string = "Hello, part 2.";
-console.log(msg);
+import * as fs from 'fs';
+
+const input = fs.readFileSync('test.txt', 'utf-8');
+const lines = input.split('\r\n');
+
+let total: number = 0;
+
+let lineCount: number = -1;
+for (const line of lines) {
+  lineCount++;
+
+  let charCount: number = -1;
+  for (const char of line) {
+    charCount++;
+
+    if (char != '*') continue;
+
+    let numberCheck: string[][] = [];
+
+    try {
+      for (let i = 0; i < 3; i++) {
+        numberCheck[i] = [];
+        for (let j = 0; j < 7; j++) {
+          numberCheck[i].push(lines[lineCount + i - 1][charCount + j - 3]);
+        }
+      }
+    } catch (TypeError) {}
+
+    // console.log(numberCheck);
+    
+    let numberList: string[] = [];
+    let nextNum: number = 0;
+
+    for (let i = 0; i < numberCheck.length; i++) {
+      for (let j = 0; j < numberCheck[i].length; j++) {
+
+        if (j < 2 && numberCheck[i][2] === ".") j = 3;
+        if (j === 4 && numberCheck[i][j] === ".") break;
+        if (j === 5 && numberCheck[i][j] === ".") break;
+
+        if (numberCheck[i][j] === undefined || numberCheck[i][j] === ".") continue;
+        
+        if (!isNaN(Number(numberCheck[i][j]))) {
+          numberList[nextNum] = numberCheck[i][j];
+          if (!isNaN(Number(numberCheck[i][j + 1]))) {
+            j++;
+            numberList[nextNum] += numberCheck[i][j];
+            if (!isNaN(Number(numberCheck[i][j + 1]))) {
+              j++;
+              numberList[nextNum] += numberCheck[i][j];
+              nextNum++;
+            }
+            else {
+              nextNum++;
+            }
+          }
+          else {
+            nextNum++;
+          }
+        }
+        
+      }
+    }
+    if (numberList.length < 2) continue;
+    total += Number(numberList[0]) * Number(numberList[1]);
+  }
+}
+
+console.log(total);
+
+// 73662618 too low
+
