ba9c9f8329ab2add732ed3e6ca9119c9d6a5adbe
NodeNebula <node.nebula@gmail.com>
2023-12-06 18:37:31 +0100

    Day 1 part 1, comments

--- Day 1 - Java/Advent of Code 2023 - Day 1/src/PartOne.java
+++ Day 1 - Java/Advent of Code 2023 - Day 1/src/PartOne.java
@@ -5,27 +5,38 @@ import java.util.Scanner;
 public class PartOne {
     public static void main(String[] args) {
         try {
+            // Get puzzle input
             File puzzleInput = new File("src/input.txt");
             Scanner fileReader = new Scanner(puzzleInput);
 
+            // Setup end value
             int total = 0;
 
+            // Read file line by line
             while (fileReader.hasNextLine()) {
+                // Set line to string value
                 String line = fileReader.nextLine();
 
+                // Setup temp value
                 String stringNumber = "";
 
+                // Go through all chars
                 for (int i = 0; i < line.length(); i++) {
+                    // Check if char is int
                     if (Character.isDigit(line.charAt(i))) {
+                        // Add int to string line
                         stringNumber += line.charAt(i);
                     }
                 }
 
+                // Get first and last digit and set to string
                 String currentNumber = String.valueOf(stringNumber.charAt(0)) +
                         String.valueOf(stringNumber.charAt(stringNumber.length() - 1));
+                // Add number onto total
                 total += Integer.parseInt(currentNumber);
             }
 
+            // Output total
             System.out.println(total);
 
         } catch (FileNotFoundException e) {
